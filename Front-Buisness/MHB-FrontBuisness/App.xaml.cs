302f8abb00a75b790e3b2e56803494769f8859e1
David <bodratomorenodavid@gmail.com>
2023-12-15 18:57:25 +0100

    Newly created projects

---
+++ Front-Buisness/MHB-FrontBuisness/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MHB_FrontBuisness
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

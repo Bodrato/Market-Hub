11245a5b5a511222a4231e8cf0dd9c84ebb270b0
Juan José Fernández Moreno <jjmateo1279@gmail.com>
2024-01-15 20:14:29 +0100

    create markethub-back

--- markethub-back/src/main/java/com/markethub/smarkethubback/MarkethubBackApplication.java
+++ markethub-back/src/main/java/com/markethub/smarkethubback/MarkethubBackApplication.java
@@ -1,13 +1,13 @@
-package com.markethub;
+package com.markethub.smarkethubback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarketHubApplication {
+public class MarkethubBackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MarketHubApplication.class, args);
+		SpringApplication.run(MarkethubBackApplication.class, args);
 	}
 
 }
--- markethub-back/src/test/java/com/markethub/smarkethubback/MarkethubBackApplicationTests.java
+++ markethub-back/src/test/java/com/markethub/smarkethubback/MarkethubBackApplicationTests.java
@@ -1,10 +1,10 @@
-package com.markethub;
+package com.markethub.smarkethubback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MarketHubApplicationTests {
+class MarkethubBackApplicationTests {
 
 	@Test
 	void contextLoads() {

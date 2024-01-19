7b4b875cc75cfcbb103091fceeae4a6d62b00f31
VictorEsper <victoresclapespertusa@gmail.com>
2024-01-19 19:02:44 +0100

    register v01

--- MarketHub/src/app/register/register.component.ts
+++ MarketHub/src/app/register/register.component.ts
@@ -1,12 +1,54 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm = new FormGroup({
+    nickname: new FormControl('',[Validators.required]),
+    realName: new FormControl('',Validators.required),
+    address: new FormControl('',Validators.required),
+    email: new FormControl('',[Validators.required,Validators.email]),
+    phone: new FormControl('',[Validators.required,Validators.pattern("[0-9 ]{11}")]),
+    password: new FormControl('',Validators.required)
+  });
 
+  register() {
+    if(this.registerForm.invalid) return;
+    alert('hola');
+  }
+
+  get nickname() {
+    return this.registerForm.controls.nickname
+  }
+
+  get realName() {
+    return this.registerForm.controls.realName
+  }
+
+  get address() {
+    return this.registerForm.controls.address
+  }
+
+  get email() {
+    return this.registerForm.controls.email
+  }
+
+  get phone() {
+    return this.registerForm.controls.phone
+  }
+
+  get password() {
+    return this.registerForm.controls.password
+  }
 }
+
+
+
+

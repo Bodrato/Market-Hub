ef7b9a8147c780a058f45201ca6c2dea973db35d
VictorEsper <victoresclapespertusa@gmail.com>
2024-01-19 18:34:03 +0100

    Register route added

--- MarketHub/src/app/app.routes.ts
+++ MarketHub/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
-    { path: 'home', component: MainComponent }
+    { path: 'home', component: MainComponent },
+    { path: 'register', component: RegisterComponent },
 ];

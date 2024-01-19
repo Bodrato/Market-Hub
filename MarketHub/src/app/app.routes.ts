e5a0432a8e9901c410f7a27e5f6e4509a02ef2e3
Bodrato <bodratomorenodavid@gmail.com>
2024-01-19 19:13:05 +0100

    route added

--- MarketHub/src/app/app.routes.ts
+++ MarketHub/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: 'home', component: MainComponent },
-
-    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent }
 ]

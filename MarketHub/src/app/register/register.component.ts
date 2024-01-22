d645dc52335d2d20d27f8ed2d170fbccf7d3c53b
Bodrato <bodratomorenodavid@gmail.com>
2024-01-22 23:29:07 +0100

    apiService added

--- MarketHub/src/app/register/register.component.ts
+++ MarketHub/src/app/register/register.component.ts
@@ -1,27 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, HttpClientModule],
+  providers: [ApiService],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  constructor(private apiService: ApiService, public router: Router) { }
+
   registerForm = new FormGroup({
     nickname: new FormControl('',[Validators.required]),
     realName: new FormControl('',Validators.required),
     address: new FormControl('',Validators.required),
     email: new FormControl('',[Validators.required,Validators.email]),
-    phone: new FormControl('',[Validators.required,Validators.pattern("[0-9 ]{11}")]),
+    phone: new FormControl('',[Validators.required]),
     password: new FormControl('',Validators.required)
   });
 
   register() {
     if(this.registerForm.invalid) return;
-    alert('hola');
+    
+    const account = this.registerForm.value as Record<string, string>;
+
+    this.apiService.saveAccount(account).subscribe({
+      next: (data) => { alert(data) },
+      complete: () => { this.router.navigate(['home']) },
+      error: (e) => { alert(e) }
+    });
+
   }
 
   get nickname() {

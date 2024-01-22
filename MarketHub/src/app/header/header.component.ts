e27acfddbb1278e6a5974dc967993337fba9b335
Bodrato <bodratomorenodavid@gmail.com>
2024-01-22 23:26:23 +0100

    add import MatFormFieldModule

--- MarketHub/src/app/header/header.component.ts
+++ MarketHub/src/app/header/header.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatFormFieldModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })

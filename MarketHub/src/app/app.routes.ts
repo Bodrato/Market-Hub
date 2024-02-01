009c37f3e1c498dad7c89c5d889d9ce911877393
Bodrato <bodratomorenodavid@gmail.com>
2024-02-01 15:58:06 +0100

    Test route item-view added

--- MarketHub/src/app/app.routes.ts
+++ MarketHub/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { MainComponent } from './main/main.component';
 
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { ItemViewComponent } from './item-view/item-view.component';
 
 export const routes: Routes = [
     { path: 'home', component: MainComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'item-view', component: ItemViewComponent },
     { path: '', component: MainComponent }
 ]
